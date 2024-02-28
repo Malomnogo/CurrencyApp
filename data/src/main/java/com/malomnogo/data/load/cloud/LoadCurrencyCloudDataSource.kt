f4b0ae7511770e691a77e0ab7e4f3cb0e978071f
Eugene <auricamercer@gmail.com>
2024-02-28 21:59:25 +0300

    fix LoadCurrencyCloudDataSource

--- data/src/main/java/com/malomnogo/data/load/cloud/LoadCurrencyCloudDataSource.kt
+++ data/src/main/java/com/malomnogo/data/load/cloud/LoadCurrencyCloudDataSource.kt
@@ -1,27 +1,14 @@
 package com.malomnogo.data.load.cloud
 
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 interface LoadCurrencyCloudDataSource {
 
     suspend fun currencies(): HashMap<String, String>
 
     class Base(private val currencyService: CurrencyService) : LoadCurrencyCloudDataSource {
-        constructor() : this(
-            Retrofit.Builder().baseUrl("https://api.frankfurter.app/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(
-                    OkHttpClient.Builder()
-                        .addInterceptor(HttpLoggingInterceptor().apply {
-                            setLevel(HttpLoggingInterceptor.Level.BODY)
-                        })
-                        .build()
-                )
-                .build()
-                .create(CurrencyService::class.java)
+        constructor(retrofit: Retrofit) : this(
+            retrofit.create(CurrencyService::class.java)
         )
 
         override suspend fun currencies() = currencyService.currencies().execute().body()!!
