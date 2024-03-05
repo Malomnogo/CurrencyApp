612a7528630be89224fafa6de9063cd070be343d
Eugene <auricamercer@gmail.com>
2024-03-05 18:46:09 +0300

    MC-12 Modules add necessary modules to create viewModels and run app

--- app/src/main/java/com/malomnogo/modules/ProvideModule.kt
+++ app/src/main/java/com/malomnogo/modules/ProvideModule.kt
@@ -6,6 +6,7 @@ import com.malomnogo.presentation.dashboard.DashboardViewModel
 import com.malomnogo.presentation.load.LoadViewModel
 import com.malomnogo.presentation.main.Clear
 import com.malomnogo.presentation.main.MainViewModel
+import com.malomnogo.presentation.settings.SettingsViewModel
 
 interface ProvideModule {
 
@@ -22,6 +23,7 @@ interface ProvideModule {
                 MainViewModel::class.java -> MainModule(core)
                 LoadViewModel::class.java -> LoadModule(core, clear)
                 DashboardViewModel::class.java -> DashboardModule(core, clear)
+                SettingsViewModel::class.java -> SettingsModule(core, clear)
                 else -> throw IllegalStateException("unknown viewModel $clazz")
             } as Module<T>
         }
